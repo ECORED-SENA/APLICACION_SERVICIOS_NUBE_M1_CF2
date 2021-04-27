86f5933b7c16525a8e8c7c3f9b790aac9c38cf49
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2021-04-26 23:55:01 -0500

    Terminado
    Componente formativo listo para revisión.

---
+++ src/views/Introduccion.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.curso-main-container.introduccion
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  
+    .titulo-principal
+      .titulo-principal__numero
+        .h3
+          i.fas.fa-info
+      .h3 Introducción
+    
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+    p.mb-4 A través de este material, se tratan con detalle los pasos que se deben seguir en el proceso de recolección de datos, el uso de técnicas y los instrumentos para tal fin.
+    p.mb-4 La recolección de datos se refiere al uso de una gran diversidad de técnicas y herramientas que pueden ser utilizadas por el analista para desarrollar los sistemas de información, las cuales pueden ser la entrevista, la encuesta, el cuestionario, la observación, las sesiones grupales, la recolección documental, entre otras.
+    p En ese sentido, los analistas utilizan una variedad de métodos para recopilar los datos sobre una situación existente, como entrevistas, cuestionarios, inspección de registros (revisión en el sitio) y observación. Cada uno tiene ventajas y desventajas; es por ello que, por lo general, se utilizan dos o tres simultáneamente, para complementar el trabajo asegurar una investigación completa.
+
+</template>
+
+<script>
+import BannerInterno from '../components/plantilla/BannerInterno'
+export default {
+  name: 'Introduccion',
+  components: {
+    BannerInterno,
+  },
+  data: () => ({
+    globalData: global,
+  }),
+}
+</script>
+
+<style lang="sass" scoped>
+.introduccion
+</style>
